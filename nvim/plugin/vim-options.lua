8dc8d36ca28bc5d0667a681cbcfb6e5f52f41879
raiesbo <raiesbo@gmail.com>
2026-04-12 12:29:08 +0200

    fix nvim loading error and extend gitignore

--- nvim/plugin/vim-options.lua
+++ nvim/plugin/vim-options.lua
@@ -2,12 +2,6 @@ local opt = vim.opt
 
 ----- General Options -----
 
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
